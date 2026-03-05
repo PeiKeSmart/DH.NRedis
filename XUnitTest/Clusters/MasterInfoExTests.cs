75d983a3c300d29a5b4eece557394e91cce5cb84
猿人易 <qcjxberin@outlook.com>
2026-03-05 16:56:09 +0800

    补充完善单元测试覆盖，新增16个测试文件共62个测试用例

--- XUnitTest/Clusters/MasterInfoExTests.cs
+++ XUnitTest/Clusters/MasterInfoExTests.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using NewLife.Caching.Clusters;
 using Xunit;
 
@@ -53,5 +53,6 @@ public class MasterInfoExTests
         var inf = new MasterInfo();
         var result = inf.ToString();
         Assert.NotNull(result);
+        Assert.Contains("MasterInfo", result);
     }
 }
